908b79678859f4820593fcaedfa43b6e2cfd62a9
Djalma35 <70827881+Djalma35@users.noreply.github.com>
2021-07-29 16:27:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import api
+
+
+def main():
+    api.clear()
+    api.banner()
+    print("""
+1) IP
+2) IP 2
+3) CEP
+4) CEP 2
+5) CNPJ
+6) Banco
+7) COVID-19
+8) WHOIS
+S) Sair
+    """)
+    main_input = input("Escolha uma opção: ")
+    if main_input in "1":
+        api.clear()
+        api.ip()
+
+    elif main_input in "2":
+        api.clear()
+        api.ip2()
+
+    elif main_input in "3":
+        api.clear()
+        api.cep()
+
+    elif main_input in "4":
+        api.clear()
+        api.cep2()
+
+    elif main_input in "5":
+        api.clear()
+        api.cnpj()
+
+    elif main_input in "6":
+        api.clear()
+        api.banks()
+
+    elif main_input in "7":
+        api.clear()
+        api.covid19()
+
+    elif main_input in "8":
+        api.clear()
+        api.whois()
+
+    elif main_input in "Ss":
+        api.clear()
+
+    else:
+        api.clear()
+        print("Opção invalida.")
+
+
+if __name__ == '__main__':
+    main()
