5f90361a685fd2ec8f6f1b562df7d90f7e86d417
Djalma35 <70827881+Djalma35@users.noreply.github.com>
2021-08-05 11:33:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import time
 import telebot
 import os
-from pytube import YouTube
 from id_user import UserID
 from instagram import Insta, instafoto
 from ip import IP
@@ -24,23 +23,7 @@ bot = telebot.TeleBot(tk, parse_mode="markdown")
 
 @bot.message_handler(commands=['start', 'help', 'comesar', 'acorda'])
 def welcome(message):
-    bot.reply_to(message, '*Olá, sou um bot feito em Python criado pelo @NearShelby_yt tenho varias funções porem a mais usada é a /yt, esse comando baixa o video que você desejar e te manda \n\nObs: Videos longos demoram muito para baixar ou as vezes nem baixam\n\n basta digitar o comando /yt url*\n\n_Exemplo:_\n\n/yt `https://www.youtube.com/watch?v=iJ_pigL5-Lo`\n\n*Outros comandos:**\n\n/criador Youtube e github do criador \n\n/apis Lista de apis\n\n/dpwb Alguns links da Deep Web\n\n/lokicpf Obter comandos para instalar a tool LokiCpf\n\n/id O bot ira te identificar\n\n/insta Dados de um perfil no Instagram\n\n/ip Consultar ip\n\n/gercpf Gerar CPF \n\n/gercnpj Gera um CNPJ\n\n/placa Consultar placa\n\n/scan Portscan\n\n/cep Consultar CEP\n\n/cnpj Consultar CNPJ\n\n/md5 Criptografar em MD5\n\n/gercns Gerar CNS\n\n/banks Consultar codigo bancario\n\n/bin Consultar BIN\n\n/ddd Consultar DDD\n\nCriador: @NearShelby_yt*')
-
-@bot.message_handler(commands=['criador'])
-def ns(message):
-    bot.reply_to(message, '*GitHub:* `https://github.com/nearshelby-yt`\n*Youtube:* `https://www.youtube.com/channel/UCYx02EM3e2h2Nbn2OwJ9voQ`\n')
-
-@bot.message_handler(commands=['apis'])
-def apis(message):
-    bot.reply_to(message, '*Algumas apis:*\n\nhttps://api.hashify.net/hash/md5/hex?value=aquificaoquedesejacodificar \n\nhttps://api.hashify.net/hash/md4/hex?value=aquificaoquedesejacodificar\n\nhttps://viacep.com.br/ws/01001000/json/\n\nhttps://www.receitaws.com.br/v1/cnpj/ \n\nhttp://ip-api.com/json/ \n\nhttp://104.41.5.41:12345/cpf.php?lista={} \n\nhttps://binlist.io/lookup/450570/\n\nhttps://ipwhois.app\n\nhttps://github.com/100n0m3/API-Carros')
-
-@bot.message_handler(commands=['dpwb'])
-def deepweb(message):
-    bot.reply_to(message, '*Alguns links da deep web*\n\n`http://sigaintbulkwy2vl.onion/\n\nhttp://hiddenchaty5hhgj.onion/\n\nhttp://hss3uro2hsxfogfq.onion/\n\nhttp://hi2vavcolvubv6an.onion/\n\nhttp://highkorea5ou4wcy.onion/\n\nhttp://ippxb64tbjxzltkf.onion/`')
-
-@bot.message_handler(commands=['lokicpf'])
-def lokicpf(message):
-    bot.reply_to(message, '*Comandos para instalar o LokiCpf que é uma tool de consultar cpf feita em Python pelo NearShelby*\n\n\n*Debian|Ubunto e derivados*\n\napt install git\n\napt install python && apt install python3\n\napt install python-pip3\n\ngit clone https://github.com/nearshelby-yt/LokiCpf\n\ncd LokiCpf\n\npip3 install -r requirements.txt\n\npython3 main.py\n\n\n*Termux*\n\napt install git\n\napt install python && apt install python2\n\ngit clone https://github.com/nearshelby-yt/LokiCpf\n\ncd LokiCpf\n\npip install -r requirements.txt\n\npython3 main.py')
+    bot.reply_to(message, '*Olá, sou um bot feito em Python criado pelo @slayersh')
 
 @bot.message_handler(commands=['id'])
 def reply(message):
